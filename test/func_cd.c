f05a72eed107736dc9b08de1abe1a9f2aa2c63fd
zml <zmlii23@163.com>
2024-07-24 20:38:03 +0800

    client.c

--- test/func_cd.c
+++ test/func_cd.c
@@ -1,27 +1,11 @@
 #include <func.h>
 
-enum cd_retval
+int main(int argc,char* argv)
 {
-    CD_SUCCESS,
-    ARGS_ILLEGAL,
-    NO_SUCH_DIR
-};
-int func_cd(int netfd)
-{
-    enum cd_retval cdret;
-    int rdset = read(netfd,&cdret,sizeof(cdret));
-    ERROR_CHECK(rdset,-1,"cd_client_read");
-    if(cdret == CD_SUCCESS)
-    {
-        printf("cd_success!\n");
-    }
-    else if(cdret == ARGS_ILLEGAL)
-    {
-        printf("command  args is illegal!\n");
-    }
-    else
-    {
-        printf("no such dir!\n");
-    }
-    return 0;
+    //./test "cd dir1/subdir1"
+    ARGS_CHECK(argc,2);
+    char str[] = "cd dir1";
+    
+    
+
 }
